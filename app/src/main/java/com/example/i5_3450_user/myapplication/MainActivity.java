a188969a27deeebb80f65b23fa9d95c326f97bd8
lonelyversemaker <xperiasesk17@gmail.com>
2017-06-29 10:53:21 +0500

    - added button, textViewInfo, editText for future experiments

--- app/src/main/java/com/example/i5_3450_user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/i5_3450_user/myapplication/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.i5_3450_user.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private EditText mResultEditText;
+    private TextView mInfoTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = (Button)findViewById(R.id.buttonGetResult);
+        mResultEditText = (EditText)findViewById(R.id.editText);
+        mInfoTextView = (TextView)findViewById(R.id.textViewInfo);
+    }
+
+    public void onClick(View view) {
+        mResultEditText.setText("Test");
     }
 }
